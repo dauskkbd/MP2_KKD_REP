8ac7bd0ced73fcbe13e168a0c8a310458e132209
dauskkbd <dauskkbd@gmail.com>
2024-01-22 20:55:23 +0800

    removed unwanted files

--- Express copy/approach1/js/navbar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $(".genre").click(function () {
-    let genre = $(this).text();
-    sessionStorage.setItem("genre_selected", genre);
-    window.location.href = "genre.html";
-  });
-});
